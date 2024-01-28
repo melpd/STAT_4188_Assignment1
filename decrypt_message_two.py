4d9bf69ba2a03014b82906136667a136368309fb
melpd <melanie.desroches@uconn.edu>
2024-01-28 15:10:12 -0500

    Wrote the code for decrypting message two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -1,7 +1,19 @@
-encrypted_file = open("encrypted_message_two.txt", 'r')
+#encrypted_file = open("encrypted_message_two.txt", 'r')
 
-encrypted_message = encrypted_file.readline()
+#encrypted_message = encrypted_file.readline()
 
-encrypted_file.close()
+#encrypted_file.close()
 
 # Write Code Here
+encrypted_message = 'Tgeoq ooe bb oo  vohwtynis,o" niissaemwsth  unrhn  oa aaswion eamntioaoaopiesni  dhvtrt eeiip cm EfDcEn eutyan  w"rtnssa as uedwres ieacee nipcd ehi irnbseg isnmtcr douefc mcllxr  tttdmesewp onno rocdls bqnaieacurcehaii nowttl raos ttou eayet enriccimel te ia aAo. eapkoo  rnmehsua dinm leosptevt eenesddntimstbeteenn dits eeesa.aIl uoxrtRoA Mo.tdafml ,hp oaitesa  exgllndthoe  scte ch ttydutteirkcthii  euns  tAd es  oa laGemtT ye ttoul nh.'
+encrypted_list = [c for c in encrypted_message]
+decrypted_message = ''
+for l in range(0, len(encrypted_list)//2):
+    if l % 2 == 1:
+        place_holder = encrypted_list[l]
+        encrypted_list[l] = encrypted_list[-l-1]
+        encrypted_list[-1-l] = place_holder
+decrypted_message= ''.join(encrypted_list)
+print(decrypted_message)
+
+        
